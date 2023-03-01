05baa52f1e09ece36483459d1d004da2f655e4b2
Aviad Bitton <aviadbi1@gmail.com>
2023-03-01 13:22:01 +0200

    add cors support

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const ClassSampler = require("./classSampler");
 
 const classSampler = new ClassSampler();
 
 const app = express();
+app.use(cors());
 
 app.get("/sampleClasses", async (req, res) => {
   try {
+    console.log("got request");
     const wordCloud = await classSampler.generateWordCloud();
     res.status(200).send(JSON.stringify(wordCloud));
   } catch (error) {
